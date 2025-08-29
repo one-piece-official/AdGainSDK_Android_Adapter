8ccb8021c8377e8fe21129ffa74577a6bab38a3a
wutianlong <804098948@qq.com>
2025-08-29 14:35:30 +0800

    tobid  4.2.2

---
+++ tobid/adgain-adapter/src/main/java/com/tobid/adapter/adgain/AdGainCustomerBanner.java
@@ -0,0 +1,114 @@
+package com.tobid.adapter.adgain;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+
+import com.adgain.sdk.api.AdError;
+import com.adgain.sdk.api.AdRequest;
+import com.adgain.sdk.api.BannerAd;
+import com.adgain.sdk.api.BannerAdListener;
+import com.windmill.sdk.WMConstants;
+import com.windmill.sdk.WindMillError;
+import com.windmill.sdk.base.WMAdapterError;
+import com.windmill.sdk.custom.WMCustomBannerAdapter;
+import com.windmill.sdk.models.BidPrice;
+
+import java.util.Map;
+
+public class AdGainCustomerBanner extends WMCustomBannerAdapter implements BannerAdListener {
+    private static final String TAG = "AdGainBanner";
+
+    BannerAd mBannerAd;
+
+    @Override
+    public void loadAd(Activity activity, Map<String, Object> localExtra, Map<String, Object> serverExtra) {
+        Log.d(TAG, "loadAd: l: " + localExtra + " s: " + serverExtra + "  " + AdGainAdapterUtil.getBidFloor(this, serverExtra));
+
+        try {
+            String codeId = (String) serverExtra.get(WMConstants.PLACEMENT_ID);
+
+            AdRequest adRequest = new AdRequest.Builder()
+                    .setCodeId(codeId)
+                    .build();
+
+            mBannerAd = new BannerAd(adRequest, this, true, true);
+
+            mBannerAd.loadAd();
+        } catch (Exception tr) {
+            Log.e(TAG, "loadAd exception: ", tr);
+            callLoadFail(new WMAdapterError(WindMillError.ERROR_AD_ADAPTER_LOAD.getErrorCode(),
+                    "catch AdGain loadAd error " + Log.getStackTraceString(tr)));
+        }
+
+    }
+
+    @Override
+    public boolean isReady() {
+        boolean result = mBannerAd != null && mBannerAd.isReady();
+        Log.d(TAG, "banner isReady = " + result);
+        return result;
+    }
+
+    @Override
+    public void destroyAd() {
+        Log.d(TAG, "destroyAd ");
+        if (mBannerAd != null) {
+            mBannerAd.destroyAd();
+        }
+
+
+    }
+
+    @Override
+    public View getBannerView() {
+        Log.d(TAG, "getBannerView ");
+        if (mBannerAd != null && isReady()) {
+            return mBannerAd.getBannerView();
+        }
+        return null;
+    }
+
+    @Override
+    public void onBannerAdLoadSuccess() {
+        Log.d(TAG, "onBannerAdLoadSuccess");
+        if (mBannerAd != null && getBiddingType() == WMConstants.AD_TYPE_CLIENT_BIDING) {
+            BidPrice bidPrice = new BidPrice(String.valueOf(mBannerAd.getBidPrice()));
+            callLoadBiddingSuccess(bidPrice);
+        }
+        callLoadSuccess();
+
+    }
+
+    @Override
+    public void onBannerAdShow() {
+        Log.d(TAG, "onBannerAdShow");
+        callBannerAdShow();
+    }
+
+    @Override
+    public void onBannerAdClick() {
+        Log.d(TAG, "onBannerAdClick");
+        callBannerAdClick();
+    }
+
+    @Override
+    public void onBannerAdClosed() {
+        Log.d(TAG, "onBannerAdClosed");
+        callBannerAdClosed();
+    }
+
+    @Override
+    public void onBannerAdLoadError(AdError adError) {
+        Log.d(TAG, "onBannerAdLoadError " + adError);
+        callLoadFail(new WMAdapterError(WindMillError.ERROR_AD_ADAPTER_LOAD.getErrorCode(),
+                adError.getMessage()));
+
+    }
+
+    @Override
+    public void onBannerAdShowError(AdError adError) {
+        Log.d(TAG, "onBannerAdShowError " + adError);
+
+    }
+}
